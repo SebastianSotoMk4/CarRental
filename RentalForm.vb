d2f289eb3947648f7e6dc18539b084890ca23747
Sebastian Soto <thefirstcoolone@gmail.com>
2021-10-25 08:55:07 -0600

    added code and header

--- RentalForm.vb
+++ RentalForm.vb
@@ -1,6 +1,44 @@
-﻿Option Explicit On
+﻿'Sebastian Soto
+'RCET0265
+'Fall 2021
+'Car Rental
+'https://github.com/SebastianSotoMk4/CarRental.git
+
+Option Explicit On
 Option Strict On
 Option Compare Binary
 Public Class RentalForm
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        'Closes program when clicked
+        Me.Close()
+    End Sub
+    'spellcheck
+    Sub SetDefaults()
+        NameTextBox.Text = ""
+        AddressTextBox.Text = ""
+        CityTextBox.Text = ""
+        StateTextBox.Text = ""
+        ZipCodeTextBox.Text = ""
+        BeginOdometerTextBox.Text = ""
+        EndOdometerTextBox.Text = " "
+        DaysTextBox.Text = ""
+        TotalMilesTextBox.Text = ""
+        MileageChargeTextBox.Text = ""
+        DayChargeTextBox.Text = ""
+        TotalDiscountTextBox.Text = ""
+        TotalChargeTextBox.Text = ""
+        MilesradioButton.Checked = True
+        KilometersradioButton.Checked = False
+        AAAcheckbox.Checked = False
+        Seniorcheckbox.Checked = False
 
+    End Sub
+
+    Private Sub RentalForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        SetDefaults()
+    End Sub
+
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        SetDefaults()
+    End Sub
 End Class
