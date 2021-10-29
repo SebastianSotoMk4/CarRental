7ce7c81017910bbadda1ea56b173cad85c6390fc
Sebastian Soto <thefirstcoolone@gmail.com>
2021-10-29 09:13:16 -0600

    added miles and day calculations

--- RentalForm.vb
+++ RentalForm.vb
@@ -36,27 +36,50 @@ Public Class RentalForm
         Seniorcheckbox.Checked = False
     End Sub
     Sub AddMiles()
-        Dim beginingMiles As Integer
-        Dim endMiles As Integer
         Dim totalMiles As Integer
-        Dim amoutOwed As Double
-        beginingMiles = CInt(BeginOdometerTextBox.Text)
-        endMiles = CInt(EndOdometerTextBox.Text)
+        Dim mileConvert As Double
+        Dim milesCharge As Double
+        totalMiles = CInt(EndOdometerTextBox.Text) - CInt(BeginOdometerTextBox.Text)
+        If KilometersradioButton.Checked = True Then
+            For u = 0 To totalMiles
+                mileConvert += 0.621371
+            Next
+            mileConvert = Math.Ceiling(mileConvert)
+            totalMiles = CInt(mileConvert)
+        End If
 
-        Select Case endMiles - beginingMiles
-            Case < 200
-            Case > 201
-                totalMiles = 200 - (endMiles - beginingMiles)
-                amoutOwed = 0.12 * totalMiles
-        End Select
 
+        totalMiles -= 200
+        If totalMiles >= 0 Then
+            For i = 1 To totalMiles
+                If i > 500 Then
+                    milesCharge += 0.1
+                ElseIf i <= 500 Then
+                    milesCharge += 0.12
+                End If
+            Next
+        End If
+        MileageChargeTextBox.Text = milesCharge.ToString("c")
+    End Sub
+    Sub AddDays()
+        Dim days As Integer
+        Dim dayCharge As Integer = 15
+        days = CInt(DaysTextBox.Text)
+        dayCharge = days * dayCharge
+        DayChargeTextBox.Text = CStr(dayCharge)
 
     End Sub
+
     Private Sub RentalForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         SetDefaults()
     End Sub
 
     Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
         SetDefaults()
+    End Sub
+
+    Private Sub CalculateButton_Click(sender As Object, e As EventArgs) Handles CalculateButton.Click
+        AddDays()
+        AddMiles()
     End Sub
 End Class
