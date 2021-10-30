7af4635dc3abe8c63a8086d750bb1978a941a761
Sebastian Soto <thefirstcoolone@gmail.com>
2021-10-30 15:19:01 -0600

    added functionality

--- RentalForm.vb
+++ RentalForm.vb
@@ -10,9 +10,13 @@ Option Compare Binary
 Public Class RentalForm
     Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
         'Closes program when clicked
-        MsgBox("close?", vbYesNo)
-        If vbYesNo = 4 Then
+        Dim answer As Integer
+
+        answer = MsgBox("close?", vbYesNo)
+        If answer = 6 Then
             Me.Close()
+        ElseIf answer = 7 Then
+
         End If
     End Sub
     'Sets Defaults when called
@@ -36,7 +40,6 @@ Public Class RentalForm
         Seniorcheckbox.Checked = False
     End Sub
     Function UserInputCheck() As Integer
-        Dim allValid As Boolean
         Dim testNumber As Integer
         Dim checkNumber As Integer
         Dim errorCheck As Integer
@@ -44,6 +47,7 @@ Public Class RentalForm
         Try
             testNumber = CInt(NameTextBox.Text)
             errorCheck = 1
+            NameTextBox.Text = ""
         Catch ex As Exception
             If NameTextBox.Text <> "" Then
                 checkNumber += 1
@@ -55,11 +59,13 @@ Public Class RentalForm
             checkNumber += 1
         ElseIf AddressTextBox.Text = "" Then
             errorCheck = 2
+            AddressTextBox.Text = ""
         End If
 
         Try
             testNumber = CInt(CityTextBox.Text)
             errorCheck = 3
+            CityTextBox.Text = ""
         Catch ex As Exception
             If CityTextBox.Text <> "" Then
                 checkNumber += 1
@@ -69,6 +75,7 @@ Public Class RentalForm
         Try
             testNumber = CInt(StateTextBox.Text)
             errorCheck = 4
+            StateTextBox.Text = ""
         Catch ex As Exception
             If StateTextBox.Text <> "" Then
                 checkNumber += 1
@@ -84,6 +91,7 @@ Public Class RentalForm
             End If
         Catch ex As Exception
             errorCheck = 5
+            ZipCodeTextBox.Text = ""
         End Try
 
         Try
@@ -94,29 +102,43 @@ Public Class RentalForm
             End If
         Catch ex As Exception
             errorCheck = 6
+            BeginOdometerTextBox.Text = ""
         End Try
 
         Try
             testNumber = CInt(EndOdometerTextBox.Text)
-            If EndOdometerTextBox.Text <> "" Then
+            If CInt(EndOdometerTextBox.Text) - CInt(BeginOdometerTextBox.Text) > 0 Then
                 checkNumber += 1
+            Else
+                errorCheck = 9
+                EndOdometerTextBox.Text = ""
+            End If
+            If EndOdometerTextBox.Text <> "" Then
+
             End If
         Catch ex As Exception
             errorCheck = 7
+            EndOdometerTextBox.Text = ""
         End Try
 
         Try
             testNumber = CInt(DaysTextBox.Text)
-            If DaysTextBox.Text <> "" Then
-                checkNumber += 1
+            If CInt(DaysTextBox.Text) - 45 <= -45 Or CInt(DaysTextBox.Text) - 45 >= 1 Then
+                errorCheck = 10
+            Else
+
+                If DaysTextBox.Text <> "" Then
+                    checkNumber += 1
+                End If
             End If
+
         Catch ex As Exception
             errorCheck = 8
         End Try
 
         If checkNumber = 8 Then
             errorCheck = 0
-            'allValid = True
+
         End If
         Return errorCheck
 
@@ -154,7 +176,11 @@ Public Class RentalForm
         dayCharge = days * dayCharge
         DayChargeTextBox.Text = dayCharge.ToString("c")
     End Sub
-
+    Sub AddDistance()
+        Dim totalDistance As Integer
+        totalDistance = CInt(BeginOdometerTextBox.Text) + CInt(EndOdometerTextBox.Text)
+        TotalMilesTextBox.Text = ($"{totalDistance}  mi")
+    End Sub
     Private Sub RentalForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         SetDefaults()
     End Sub
@@ -163,18 +189,16 @@ Public Class RentalForm
         SetDefaults()
     End Sub
 
+
     Private Sub CalculateButton_Click(sender As Object, e As EventArgs) Handles CalculateButton.Click
 
-        'If UserInputCheck() = True Then
-        '    AddDays()
-        '    AddMiles()
-        'ElseIf UserInputCheck = False Then
-        '    MsgBox("empty things")
-        'End If
+
+
         Select Case UserInputCheck()
             Case = 0
                 AddDays()
                 AddMiles()
+                AddDistance()
             Case = 1
                 MsgBox("Name")
             Case = 2
@@ -191,6 +215,10 @@ Public Class RentalForm
                 MsgBox("Enging Odomter")
             Case = 8
                 MsgBox("Number oF days")
+            Case = 9
+                MsgBox("Miles we inputed wrong")
+            Case = 10
+                MsgBox("invalid amout of days")
 
 
         End Select
@@ -198,3 +226,5 @@ Public Class RentalForm
 
     End Sub
 End Class
+
+'kindof a bug with Beginging odometer being 0
